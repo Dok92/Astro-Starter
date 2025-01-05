b1ccfc383f068f2911ebd18fb46d6e06ff809a79
Dok <doslovdok@gmail.com>
2025-01-05 15:57:50 +0100

    Initialized

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
-// import vercel from '@astrojs/vercel';
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel';
+// import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 import solid from '@astrojs/solid-js';
 
 export default defineConfig({
   // ...
   output: 'server',
-  adapter: netlify(),
-  // adapter: vercel({
-  //   webAnalytics: {
-  //     enabled: true,
-  //   },
-  // }),
+  // adapter: netlify(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind(), solid()],
 });
