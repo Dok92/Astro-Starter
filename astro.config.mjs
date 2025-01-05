54d578b6b1c1890ac3ba11f04ab4669036bc1d56
Dok <doslovdok@gmail.com>
2025-01-05 15:18:52 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+// import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
+import tailwind from '@astrojs/tailwind';
+import solid from '@astrojs/solid-js';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ...
+  output: 'server',
+  adapter: netlify(),
+  // adapter: vercel({
+  //   webAnalytics: {
+  //     enabled: true,
+  //   },
+  // }),
+  integrations: [tailwind(), solid()],
+});
